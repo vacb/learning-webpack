1300157f06821eec9cd093c26d39938cf932100e
Victoria Blackburn <victoria@sixtwo.tech>
2021-07-23 15:20:53 +0100

    Add Webpack config file

---
+++ webpack.config.js
@@ -0,0 +1,17 @@
+// Path comes with node, don't need to install
+const path = require("path");
+
+// To use config from this file, add to package.json
+module.exports = {
+  mode: "development",
+  // Per tutorial, devtool: "none" - for old version of webpack
+  // devtool: false,
+
+  // Default config for entry and output if no config file had been set up
+  entry: "./src/index.js",
+  output: {
+    filename: "main.js",
+    // __dirname takes current directory path for the machine, loaded with every node script
+    path: path.resolve(__dirname, "dist"),
+  },
+};
