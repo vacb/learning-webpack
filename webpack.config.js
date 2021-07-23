d644f2dd6b4652428ba2c5099fc3ee7ca8c6c465
Victoria Blackburn <victoria@sixtwo.tech>
2021-07-23 16:21:18 +0100

    Add cache-busting and HtmlWebpackPlugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 // Path comes with node, don't need to install
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 // To use config from this file, add to package.json
 module.exports = {
@@ -10,10 +11,15 @@ module.exports = {
   // Default config for entry and output if no config file had been set up
   entry: "./src/index.js",
   output: {
-    filename: "main.js",
+    filename: "main.[contenthash].js",
     // __dirname takes current directory path for the machine, loaded with every node script
     path: path.resolve(__dirname, "dist"),
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/template.html",
+    }),
+  ],
   module: {
     rules: [
       {
