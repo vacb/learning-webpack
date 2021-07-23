4138bbaa9a54b4b17664550adf0067cf8c17b7c4
Victoria Blackburn <victoria@sixtwo.tech>
2021-07-23 16:43:15 +0100

    Add prod and dev configs

--- webpack.common.js
+++ webpack.common.js
@@ -4,17 +4,8 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 // To use config from this file, add to package.json
 module.exports = {
-  mode: "development",
-  // Per tutorial, devtool: "none" - for old version of webpack
-  // devtool: false,
-
   // Default config for entry and output if no config file had been set up
   entry: "./src/index.js",
-  output: {
-    filename: "main.[contenthash].js",
-    // __dirname takes current directory path for the machine, loaded with every node script
-    path: path.resolve(__dirname, "dist"),
-  },
   plugins: [
     new HtmlWebpackPlugin({
       template: "./src/template.html",
