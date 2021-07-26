18fe87ccefb88b9a300c243aa6873283dae1e734
Victoria Blackburn <victoria@sixtwo.tech>
2021-07-26 11:54:56 +0100

    Minify HTML, CSS and JS in production

--- webpack.common.js
+++ webpack.common.js
@@ -1,6 +1,5 @@
 // Path comes with node, don't need to install
 const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 // To use config from this file, add to package.json
 module.exports = {
@@ -9,23 +8,8 @@ module.exports = {
     main: "./src/index.js",
     vendor: "./src/vendor.js",
   },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: "./src/template.html",
-    }),
-  ],
   module: {
     rules: [
-      {
-        test: /\.scss$/,
-        // Order is important - css-loader first then style-loader
-        // BUT the array loads in reverse order :S
-        use: [
-          "style-loader", //3. Inject styles into DOM
-          "css-loader", //2. Turns css into commonjs
-          "sass-loader", //1. Turns sass into css
-        ],
-      },
       {
         test: /\.html$/,
         use: ["html-loader"],
--- webpack.prod.js
+++ webpack.prod.js
@@ -2,6 +2,9 @@ const path = require("path");
 const common = require("./webpack.common");
 const { merge } = require("webpack-merge");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const TerserPlugin = require("terser-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = merge(common, {
   mode: "production",
@@ -10,5 +13,35 @@ module.exports = merge(common, {
     path: path.resolve(__dirname, "dist"),
     assetModuleFilename: "./imgs/[name].[hash].[ext]",
   },
-  plugins: [new CleanWebpackPlugin()],
+  optimization: {
+    minimizer: [
+      new TerserPlugin(),
+      new HtmlWebpackPlugin({
+        template: "./src/template.html",
+        minify: {
+          removeAttributeQuotes: true,
+          collapseWhitespace: true,
+          removeComments: true,
+        },
+      }),
+    ],
+  },
+  plugins: [
+    new MiniCssExtractPlugin({ filename: "[name].[hash].css" }),
+    new CleanWebpackPlugin(),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        // Order is important - css-loader first then style-loader
+        // BUT the array loads in reverse order :S
+        use: [
+          MiniCssExtractPlugin.loader, //3. Extract css into files
+          "css-loader", //2. Turns css into commonjs
+          "sass-loader", //1. Turns sass into css
+        ],
+      },
+    ],
+  },
 });
